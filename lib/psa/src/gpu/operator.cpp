59c5db137302d3d4e3bc83d32a2133007fade874
Konstantin Ditschuneit <konstantin.ditschuneit@merantix.com>
2021-12-16 11:05:58 +0100

    Remove unused components
    PSANet and PSPNet101 are not used in our experiments.

--- lib/psa/src/gpu/operator.cpp
+++
@@ -1,6 +0,0 @@
-#include "operator.h"
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("psamask_forward", &psamask_forward_cuda, "PSAMASK forward (GPU)");
-  m.def("psamask_backward", &psamask_backward_cuda, "PSAMASK backward (GPU)");
-}
